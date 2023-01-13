44fb88ff7bfc2ff35ee2826edf777993fa667585
kimzuni <me@kimzuni.com>
2023-01-13 16:42:22 +0900

    .

---
+++ favicons/svg-draw.js
@@ -0,0 +1,47 @@
+/*
+ * © 2023 kimzuni <hello@kimzuni.com>
+ * Create Date : 2023.01.12
+ * Latest Date : 2023.01.13
+ */
+
+
+
+const svg_draw_favicon = {
+	"name": "favicon",
+	"viewBox": "0 0 64 64",
+	"height": 64,
+	"width": 64,
+	"count": 0,
+	"code": ""
+}
+svg_draw_favicon.code = `
+	<g stroke-width="0">
+		<path d="m 0,32 h 32 v 5 l -24,22 h 24 v 5 h -32 v -5 l 24,-22 h -24 z"/>
+		<path d="m 36,24 h 24 v 3.75 l -18,16.5 h 18 v 3.75 h -24 v -3.75 l 18,-16.5 h -18 z"/>
+		<path d="m 26,4 h 16 v 2.5 l -12,11 h 12 v 2.5 h -16 v -2.5 l 12,-11 h -12 z"/>
+		<path d="m 52.8,0 h 11.2 v 1.75 l -8.4,7.7 h 8.4 v 1.75 h -11.2 v -1.75 l 8.4,-7.7 h -8.4 z"/>
+	</g>
+`;
+
+
+
+const f_svg_draw_favicon = function(svg_draw_box) {
+	svg_draw_favicon.count++;
+	const svg_draw_info = svg_draw_favicon;
+
+	let svg_draw_code = `<svg viewBox="${svg_draw_info.viewBox}" draw="${svg_draw_info.name}" fill="var(--svg-draw-color, #5cc9ff)" stroke="var(--svg-draw-color, #5cc9ff)">`;
+	if (svg_draw_info.count == 1) {
+		svg_draw_code += `<symbol id="svg-draw-${svg_draw_info.name}">${svg_draw_info.code}</symbol>`;
+	}
+	svg_draw_code += `<use href="#svg-draw-${svg_draw_info.name}"/></svg>`;
+
+	svg_draw_box.outerHTML = svg_draw_code;
+}
+
+
+
+window.addEventListener("load", function() {
+	for (let svg_draw_box of document.querySelectorAll(`svg[draw="${svg_draw_favicon.name}"]`)) {
+		f_svg_draw_favicon(svg_draw_box);
+	}
+});
